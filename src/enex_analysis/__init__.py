21384f4dce0c85b43e5cd15e5fb7d460e9b6f266
Wonjun <wonjunn.choi@gmail.com>
2026-03-07 16:32:23 +0900

    refactor(api): use explicit imports and define __all__ in __init__.py

--- src/enex_analysis/__init__.py
+++ src/enex_analysis/__init__.py
@@ -1,15 +1,229 @@
-def hello() -> str:
-    return "Hello from en-system-ex-analysis!"
+"""Energy and Exergy Analysis Engine package init."""
 
-from .calc_util import *
-from .dynamic_context import *
-from .subsystems import *
-from .enex_engine import *
-from .gas_boiler import *
-from .air_source_heat_pump_boiler import *
-from .ground_source_heat_pump_boiler import *
-from .tank_stratification_model import *
-from .electric_boiler import *
-from .gas_boiler_tank import *
-from .solar_assisted_gas_boiler import *
-from .enex_functions import *
+from .air_source_heat_pump_boiler import (
+    AirSourceHeatPumpBoiler,
+)
+from .calc_util import (
+    C2F,
+    C2K,
+    F2C,
+    GJ2J,
+    J2GJ,
+    J2MJ,
+    K2C,
+    MJ2J,
+    MW2W,
+    W2GW,
+    W2MW,
+)
+from .components.electric_heater import (
+    ElectricHeater,
+)
+from .components.fan import (
+    Fan,
+)
+from .components.pump import (
+    Pump,
+)
+from .constants import (
+    P0_PA,
+    SP,
+    T0_K,
+)
+from .dhw import (
+    build_dhw_usage_ratio,
+    calc_cold_water_temp,
+    calc_total_water_use_from_schedule,
+    make_dhw_schedule_from_Annex_42_profile,
+)
+from .dynamic_context import (
+    ControlState,
+    StepContext,
+    Subsystem,
+    SubsystemExergy,
+    determine_heat_source_on_off,
+    determine_tank_refill_flow,
+    tank_mass_energy_residual,
+)
+from .electric_boiler import (
+    ElectricBoiler,
+)
+from .enex_engine import (
+    AirSourceHeatPump_cooling,
+    AirSourceHeatPump_heating,
+    GroundSourceHeatPump_cooling,
+    GroundSourceHeatPump_heating,
+)
+from .enex_functions import (
+    G_FLS,
+    TDMA,
+    air_dynamic_viscosity,
+    air_prandtl_number,
+    calc_ASHP_cooling_COP,
+    calc_ASHP_heating_COP,
+    calc_boussinessq_mixing_flow,
+    calc_fan_power_from_dV_fan,
+    calc_GSHP_COP,
+    calc_HX_perf_for_target_heat,
+    calc_mixing_valve,
+    calc_Orifice_flow_coefficient,
+    calc_stc_performance,
+    calc_UA_from_dV_fan,
+    calc_uv_exposure_time,
+    calc_uv_lamp_power,
+    check_hp_schedule_active,
+    chi,
+    cubic_function,
+    f,
+    get_uv_params_from_turbidity,
+    linear_function,
+    print_balance,
+    quadratic_function,
+    quartic_function,
+    update_tank_temperature,
+)
+from .gas_boiler import (
+    GasBoiler,
+)
+from .gas_boiler_tank import (
+    GasBoilerTank,
+)
+from .ground_source_heat_pump_boiler import (
+    GroundSourceHeatPumpBoiler,
+)
+from .heat_transfer import (
+    TRIDIAG_MATRIX_ALGORITHM,
+    calc_h_vertical_plate,
+    calc_LMTD_counter_flow,
+    calc_LMTD_parallel_flow,
+    calc_simple_tank_UA,
+    calc_UA_tank_arr,
+    darcy_friction_factor,
+)
+from .refrigerant import (
+    calc_ref_state,
+    create_lmtd_constraints,
+    find_ref_loop_optimal_operation,
+)
+from .solar_assisted_gas_boiler import (
+    SolarAssistedGasBoiler,
+)
+from .subsystems import (
+    PhotovoltaicSystem,
+    SolarThermalCollector,
+    UVLamp,
+)
+from .tank_stratification_model import (
+    StratifiedTankTDMA,
+)
+from .thermodynamics import (
+    calc_energy_flow,
+    calc_exergy_flow,
+    calc_refrigerant_exergy,
+    convert_electricity_to_exergy,
+    generate_entropy_exergy_term,
+)
+from .visualization import (
+    plot_ph_diagram,
+    plot_th_diagram,
+    plot_ts_diagram,
+    print_simulation_summary,
+)
+from .weather import (
+    decompose_ghi_to_poa,
+    load_kma_solar_csv,
+    load_kma_T0_sol_hourly_csv,
+)
+
+__all__ = [
+    'AirSourceHeatPumpBoiler',
+    'AirSourceHeatPump_cooling',
+    'AirSourceHeatPump_heating',
+    'C2F',
+    'C2K',
+    'ControlState',
+    'ElectricBoiler',
+    'ElectricHeater',
+    'F2C',
+    'Fan',
+    'GJ2J',
+    'G_FLS',
+    'GasBoiler',
+    'GasBoilerTank',
+    'GroundSourceHeatPumpBoiler',
+    'GroundSourceHeatPump_cooling',
+    'GroundSourceHeatPump_heating',
+    'J2GJ',
+    'J2MJ',
+    'K2C',
+    'MJ2J',
+    'MW2W',
+    'P0_PA',
+    'PhotovoltaicSystem',
+    'Pump',
+    'SP',
+    'SolarAssistedGasBoiler',
+    'SolarThermalCollector',
+    'StepContext',
+    'StratifiedTankTDMA',
+    'Subsystem',
+    'SubsystemExergy',
+    'T0_K',
+    'TDMA',
+    'TRIDIAG_MATRIX_ALGORITHM',
+    'UVLamp',
+    'W2GW',
+    'W2MW',
+    'air_dynamic_viscosity',
+    'air_prandtl_number',
+    'build_dhw_usage_ratio',
+    'calc_ASHP_cooling_COP',
+    'calc_ASHP_heating_COP',
+    'calc_GSHP_COP',
+    'calc_HX_perf_for_target_heat',
+    'calc_LMTD_counter_flow',
+    'calc_LMTD_parallel_flow',
+    'calc_Orifice_flow_coefficient',
+    'calc_UA_from_dV_fan',
+    'calc_UA_tank_arr',
+    'calc_boussinessq_mixing_flow',
+    'calc_cold_water_temp',
+    'calc_energy_flow',
+    'calc_exergy_flow',
+    'calc_fan_power_from_dV_fan',
+    'calc_h_vertical_plate',
+    'calc_mixing_valve',
+    'calc_ref_state',
+    'calc_refrigerant_exergy',
+    'calc_simple_tank_UA',
+    'calc_stc_performance',
+    'calc_total_water_use_from_schedule',
+    'calc_uv_exposure_time',
+    'calc_uv_lamp_power',
+    'check_hp_schedule_active',
+    'chi',
+    'convert_electricity_to_exergy',
+    'create_lmtd_constraints',
+    'cubic_function',
+    'darcy_friction_factor',
+    'decompose_ghi_to_poa',
+    'determine_heat_source_on_off',
+    'determine_tank_refill_flow',
+    'f',
+    'find_ref_loop_optimal_operation',
+    'generate_entropy_exergy_term',
+    'get_uv_params_from_turbidity',
+    'linear_function',
+    'load_kma_T0_sol_hourly_csv',
+    'load_kma_solar_csv',
+    'make_dhw_schedule_from_Annex_42_profile',
+    'plot_ph_diagram',
+    'plot_th_diagram',
+    'plot_ts_diagram',
+    'print_balance',
+    'print_simulation_summary',
+    'quadratic_function',
+    'quartic_function',
+    'tank_mass_energy_residual',
+    'update_tank_temperature',
+]
